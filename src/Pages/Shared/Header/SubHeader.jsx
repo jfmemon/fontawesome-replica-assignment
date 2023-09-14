64870c17813a6167c47b3dc76accae9841dae399
jfmemon <jannatul15-2497@diu.edu.bd>
2023-09-15 00:42:55 +0600

    select button of sub header done

--- src/Pages/Shared/Header/SubHeader.jsx
+++ src/Pages/Shared/Header/SubHeader.jsx
@@ -1,8 +1,18 @@
 
 const SubHeader = () => {
     return (
-        <div className="container mx-auto px-20">
-            <h3>This is subheader.</h3>
+        <div className="container mx-auto md:px-36 md:pt-10 md:pb-5 py-5 px-6 ">
+            <div className="flex justify-between">
+                <div>
+                    <h3>This is subheader.</h3>
+                </div>
+                <div>
+                    <select className="select border-gray-300 border-2 w-full max-w-sm focus:border-cyan-500">
+                        <option selected>Featured</option>
+                        <option>Alphabetic</option>
+                    </select>
+                </div>
+            </div>
         </div>
     );
 };
