003805969da267c4f965cb3ec63a65cc8d699b4b
jfmemon <jannatul15-2497@diu.edu.bd>
2023-09-15 13:06:58 +0600

    subheader select button updated

--- src/Pages/Shared/Header/SubHeader.jsx
+++ src/Pages/Shared/Header/SubHeader.jsx
@@ -1,4 +1,3 @@
-
 const SubHeader = () => {
     return (
         <div className="container mx-auto md:px-36 md:pt-10 md:pb-5 py-5 px-6 ">
@@ -7,7 +6,7 @@ const SubHeader = () => {
                     <h3>This is subheader.</h3>
                 </div>
                 <div>
-                    <select className="select border-gray-300 border-2 w-full max-w-sm focus:border-cyan-500">
+                    <select className="select border-2 border-gray-300 w-full max-w-sm focus:border-x-4 focus:border-x-cyan-500">
                         <option selected>Featured</option>
                         <option>Alphabetic</option>
                     </select>
